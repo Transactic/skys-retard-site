ede02735f52590a20e0b1dc0bf220d59e8b28781
Transactic <143781240+Transactic@users.noreply.github.com>
2024-12-07 12:26:48 +1100

    Update home.js

--- home.js
+++ home.js
@@ -1,16 +1,1 @@
-window.onload = function() {
-    const isAdmin = localStorage.getItem("admin");
 
-    if (isAdmin) {
-        document.querySelectorAll('.admin-only').forEach(item => item.classList.add('show'));
-    }
-
-    document.querySelectorAll('.dashboard-item').forEach(item => {
-        item.addEventListener('mouseover', () => {
-            item.style.transform = 'scale(1.1)';
-        });
-        item.addEventListener('mouseout', () => {
-            item.style.transform = 'scale(1)';
-        });
-    });
-};
