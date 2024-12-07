a3fd2f2cd9fa7d78b8f2c0cdf8d4854e19662081
Transactic <143781240+Transactic@users.noreply.github.com>
2024-12-07 12:26:28 +1100

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,1 @@
-document.getElementById("login-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
 
-    if (username === "skyfollows" && password === "skyfollowsadmin") {
-        localStorage.setItem("admin", "true");
-        window.location.href = "home.html";
-    } else {
-        alert("Invalid login credentials.");
-    }
-});
